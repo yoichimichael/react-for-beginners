0f751994ffa0d2fcb3a26e841c0125594a2be0a5
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-01-14 15:07:09 -0500

    rename CoD directory

---
+++ catch-of-the-day_clone/build/precache-manifest.9813cd8b2d2bb04118fadbfe67dbebf7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d23b51f2f630e2187b93fa38a557248b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea0fb44269b3e0a6ab78",
+    "url": "/static/css/main.4e48adec.chunk.css"
+  },
+  {
+    "revision": "a8de50709d33e97d6c79",
+    "url": "/static/js/2.516b31e6.chunk.js"
+  },
+  {
+    "revision": "9b50f5741be789e4838182ccc7c0c6a5",
+    "url": "/static/js/2.516b31e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea0fb44269b3e0a6ab78",
+    "url": "/static/js/main.6d527a0f.chunk.js"
+  },
+  {
+    "revision": "682f9dc0a88c8bde3947",
+    "url": "/static/js/runtime-main.eb0b6bae.js"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/static/media/haymaker-webfont.e9535f20.ttf"
+  }
+]);
